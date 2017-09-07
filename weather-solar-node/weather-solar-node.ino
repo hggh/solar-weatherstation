15fa42419dc8b5ef7733422826835d480e801ef7
Jonas Genannt <jonas@brachium-system.net>
2017-09-07 21:49:03 +0200

    read voltage from solar panel

--- weather-solar-node/weather-solar-node.ino
+++ weather-solar-node/weather-solar-node.ino
@@ -10,12 +10,13 @@
 
 DHT dht(DHT22_PIN, DHTTYPE);
 RFM69 radio;
-char buffer[25] = "";
+char buffer[30] = "";
 
 ISR(WDT_vect) { Sleepy::watchdogEvent(); }
 
 void setup() {
   pinMode(DHT22_PIN, INPUT);
+  pinMode(SOLAR_POWER_PIN, INPUT);
 
   if (DEBUG == 1) {
     Serial.begin(9600);
@@ -29,27 +30,42 @@ void setup() {
   radio.encrypt(ENCRYPTKEY);
 
   dht.begin();
-  
+
+  power_adc_disable();
   power_twi_disable();
   power_timer1_disable();
   power_timer2_disable();
+}
+
+double read_voltage() {
+  power_adc_enable();
+  float val = analogRead(SOLAR_POWER_PIN);
+
+  double vin = ((val * voltage) / 1024.0) / (volt_r2 / ( volt_r1 + volt_r2));
+  power_adc_disable();
+
+  return vin;
 }
 
 void loop() {
 
   double t = dht.readTemperature();
   double h = dht.readHumidity();
+  
   char Tstr[10];
   char Hstr[10];
+  char Vstr[10];
 
   if (isnan(t) || isnan(h)) {
     delay(10);
     return;
   }
+  double v = read_voltage();
+  dtostrf(v, 3,2, Vstr);
   dtostrf(t, 3,2, Tstr);
   dtostrf(h, 3,2, Hstr);
   
-  sprintf(buffer, "%d;%s;%s", NODEID, Tstr, Hstr);
+  sprintf(buffer, "%d;%s;%s;%s", NODEID, Tstr, Hstr, Vstr);
   if (DEBUG == 1) {
     Serial.println(buffer);
   }
